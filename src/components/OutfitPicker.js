42c84e29a3ebb35e92f31e6e9a5d01d725204854
Chris <chrasse@hotmail.com>
2021-02-17 20:20:21 +0100

    Added function to get random outfit from user checkbox input

--- src/components/OutfitPicker.js
+++ src/components/OutfitPicker.js
@@ -1,5 +1,5 @@
 import { ClothesContext } from '../contexts/ClothesContext'
-import {useContext, useState, useEffect} from 'react'
+import { useContext, useState, useEffect } from 'react'
 import Shelf from '../components/Shelf'
 import styles from '../css/OutfitPicker.module.css'
 
@@ -15,11 +15,7 @@ const OutfitPicker = () => {
   const accessories = clothes.filter(item => item.type === 'accessory');
 
   const [outfitItems, setOutfitItems] = useState([]);
-
-  const pickNewOutfit = (e) => {
-    e.preventDefault();
-    console.log(e.target)
-  }
+  const [randomOutfit, setRandomOutfit] = useState(null);
 
   const handleChange = e => {
     console.log(e.target.value)
@@ -31,27 +27,44 @@ const OutfitPicker = () => {
     }
   }
 
+  const getRandomItem = (itemList) => itemList[Math.floor(Math.random() * itemList.length)];
+
+  const pickNewOutfit = e => {
+    e.preventDefault();
+    console.log(e.target)
+
+    const someOutfit = [];
+
+    if (outfitItems.includes('tShirt')) {
+      someOutfit.push(getRandomItem(tShirts));
+    }
+    if (outfitItems.includes('shirt')) {
+      someOutfit.push(getRandomItem(shirts));
+    }
+    if (outfitItems.includes('sweater')) {
+      someOutfit.push(getRandomItem(sweaters));
+    }
+    if (outfitItems.includes('trousers')) {
+      someOutfit.push(getRandomItem(trousers));
+    }
+    if (outfitItems.includes('shorts')) {
+      someOutfit.push(getRandomItem(shorts));
+    }
+    if (outfitItems.includes('jacket')) {
+      someOutfit.push(getRandomItem(jackets));
+    }
+    if (outfitItems.includes('shoes')) {
+      someOutfit.push(getRandomItem(shoes));
+    }
+    if (outfitItems.includes('accessory')) {
+      someOutfit.push(getRandomItem(accessories));
+    }
+
+    console.log(someOutfit);
+    setRandomOutfit([...someOutfit]);
+  }
+
   useEffect( () => console.log(outfitItems), [outfitItems]);
-
-  /* const getRandomItem = (itemList) => {
-    const randomItem =
-      itemList[Math.floor(Math.random() * itemList.length)];
-    return randomItem;
-  }
-
-  const getRandomOutfit = () => {
-    return [
-      getRandomItem(shirts),
-      getRandomItem(trousers),
-      getRandomItem(shoes)
-    ]
-  }
-
-  const [randomOutfit, setRandomOutfit] = useState(null);
-
-  const pickNewOutfit = () => {
-    setRandomOutfit(getRandomOutfit())
-  } */
 
   return (
     <div className={styles.outfitContainer}>
@@ -73,6 +86,9 @@ const OutfitPicker = () => {
         <label htmlFor="accessory">Accessory</label><br />
         <button type="sumbit">Get new outfit!</button>
       </form>
+      <div>
+        {randomOutfit && randomOutfit.map(item => (<p key={item.id}>{item.description}</p>))}
+      </div>
         {/* {randomOutfit && <div><h2>Outfit for the day:</h2><Shelf itemList={randomOutfit} canBeDeleted={false} canBeEdited={false} /></div>}
       <div className={styles.outfitBtn}>
         <button onClick={pickNewOutfit}>Get new outfit</button>
