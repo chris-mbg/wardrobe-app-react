09eacf5138fa976c48dda4d62e52dce74ac30eb2
Chris <chrasse@hotmail.com>
2021-02-17 11:18:58 +0100

    Started to make form for OutfitPicker
    Want to make it possible for user to choose items for random outfit with checkboxes-input.

--- src/components/OutfitPicker.js
+++ src/components/OutfitPicker.js
@@ -1,9 +1,19 @@
-import {ClothesContext} from '../contexts/ClothesContext'
+import { ClothesContext } from '../contexts/ClothesContext'
 import {useContext, useState, useEffect} from 'react'
 import Shelf from '../components/Shelf'
 import styles from '../css/OutfitPicker.module.css'
 
 const OutfitPicker = () => {
+  const { clothes } = useContext(ClothesContext);
+  const tShirts = clothes.filter(item => item.type === 'tShirt');
+  const shirts = clothes.filter(item => item.type === 'shirt');
+  const sweaters = clothes.filter(item => item.type === 'sweater');
+  const trousers = clothes.filter(item => item.type === 'trousers');
+  const shorts = clothes.filter(item => item.type === 'shorts');
+  const jackets = clothes.filter(item => item.type === 'jacket');
+  const shoes = clothes.filter(item => item.type === 'shoes');
+  const accessories = clothes.filter(item => item.type === 'accessory');
+
   /* const {shoes} = useContext(ShoesContext);
   const {shirts} = useContext(ShirtsContext);
   const {trousers} = useContext(TrousersContext); */
@@ -32,6 +42,24 @@ const OutfitPicker = () => {
 
   return (
     <div className={styles.outfitContainer}>
+      <p>What do you want to wear today?</p>
+      <form>
+        <input type="checkbox" id="tShirt" name="tShirt" value="tShirt" />
+        <label for="tShirt">T-Shirt</label><br />
+        <input type="checkbox" id="shirt" name="shirt" value="shirt" />
+        <label for="shirt">Shirt</label><br />
+        <input type="checkbox" id="sweater" name="sweater" value="sweater" />
+        <label for="sweater">Sweater</label><br />
+        <input type="checkbox" id="trousers" name="trousers" value="trousers" />
+        <label for="trousers">Trousers</label><br />
+        <input type="checkbox" id="shorts" name="shorts" value="shorts" />
+        <label for="shorts">Shorts</label><br />
+        <input type="checkbox" id="shoes" name="shoes" value="shoes" />
+        <label for="shoes">Shoes</label><br />
+        <input type="checkbox" id="accessory" name="accessory" value="accessory" />
+        <label for="accessory">Accessory</label><br />
+
+      </form>
         {/* {randomOutfit && <div><h2>Outfit for the day:</h2><Shelf itemList={randomOutfit} canBeDeleted={false} canBeEdited={false} /></div>}
       <div className={styles.outfitBtn}>
         <button onClick={pickNewOutfit}>Get new outfit</button>
